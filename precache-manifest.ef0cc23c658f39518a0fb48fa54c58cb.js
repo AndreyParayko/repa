76e1ed97869760d4ec5145d1fc45863ccd9e3d05
andrei <andrei97ex.ua>
2019-07-19 14:50:24 +0300

    Updates

---
+++ precache-manifest.ef0cc23c658f39518a0fb48fa54c58cb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7f6e75d73dbb9fdc94671409041c85",
+    "url": "/repa/index.html"
+  },
+  {
+    "revision": "f2bb70bd5a2522d0fb5b",
+    "url": "/repa/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b2484618ab7d47905529",
+    "url": "/repa/static/css/main.b4cc4f61.chunk.css"
+  },
+  {
+    "revision": "f2bb70bd5a2522d0fb5b",
+    "url": "/repa/static/js/2.de827483.chunk.js"
+  },
+  {
+    "revision": "b2484618ab7d47905529",
+    "url": "/repa/static/js/main.3e0482b6.chunk.js"
+  },
+  {
+    "revision": "b11873df5b336a150662",
+    "url": "/repa/static/js/runtime~main.6fed7735.js"
+  },
+  {
+    "revision": "27d7775874d98c06edb93cb2093da20f",
+    "url": "/repa/static/media/Book.27d77758.png"
+  },
+  {
+    "revision": "07fa811be4b0a0162a8bfecd5866b384",
+    "url": "/repa/static/media/Gif.07fa811b.gif"
+  },
+  {
+    "revision": "9882bb678ce3789963d29d4cf7c8d114",
+    "url": "/repa/static/media/bg.9882bb67.jpg"
+  },
+  {
+    "revision": "b87d15757dbdadd92afedc58d8e63371",
+    "url": "/repa/static/media/error.b87d1575.gif"
+  },
+  {
+    "revision": "daecacdc2e7db5b1a43e8a6c93732f5e",
+    "url": "/repa/static/media/home.daecacdc.png"
+  },
+  {
+    "revision": "5b3a3d8205ab28cf6a4d0a4f29f04b6b",
+    "url": "/repa/static/media/logo.5b3a3d82.png"
+  }
+]);
